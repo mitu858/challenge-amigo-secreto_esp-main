e20e06c5ff0717a9c1a0abdf9fd77eda3b257ebf
jdelafuente858 <jdelafuente858@gmail.com>
2025-08-20 00:24:23 -0300

    Función para agregar un amigo a la lista

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // Array global donde se almacenarán los amigos
 let amigos = [];
+
+// Función para agregar un amigo a la lista
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    // eliminamos espacios en blanco
+    const nombre = input.value.trim(); 
+    if (nombre !== "") {
+        // agregamos al array
+        amigos.push(nombre);
+        // limpiamos campo de texto
+        input.value = "";
+        // refrescamos la lista en pantalla
+        actualizarLista(); 
+    } 
+    else {
+        alert("Por favor, inserte un nombre.");
+    }
+
+}
